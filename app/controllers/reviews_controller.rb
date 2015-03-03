7622b40def1e22cf8a560d71041a0b2c4d07a30d
rostyslav-m <ros55@ukr.net>
2015-03-03 09:34:44 +0200

    udate layouts/application

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,13 +4,13 @@ class ReviewsController < ApplicationController
 #, only: [:show, :edit, :update, :destroy]
 
   def create
-#    @product = Product.find(params[:product_id])
-    @review = @product.reviews.create(review_params)
+    @review = @product.reviews.new(review_params)
+#    @review.user = current_user
+    @review.save
     redirect_to product_path(@product)
   end
 
   def destroy
-#    @product = Product.find(params[:product_id])
     @review = @product.reviews.find(params[:id])
     @review.destroy
     redirect_to product_path(@product)
